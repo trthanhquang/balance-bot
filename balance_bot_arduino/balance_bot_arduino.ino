6b99cecd39257d5c7c72bdce2d3f585918153d50
Louis <trthanhquang@gmail.com>
2015-05-13 00:33:48 -0700

    faster loop by removing serial print

--- balance_bot_arduino/balance_bot_arduino.ino
+++ balance_bot_arduino/balance_bot_arduino.ino
@@ -21,13 +21,13 @@
 
 //Generals
 #define pi 3.14159 
-#define DEBUG_MODE 1 // 0:false or 1:true
+#define DEBUG_MODE 1 // comment to deactivate
 
 // System Parameters
-#define Gry_offset 273.0
+#define Gry_offset 321
 #define Gyr_Gain 0.00763358
-#define Angle_offset -1 //2.7
-#define RMotor_offset 19
+#define Angle_offset -3.95 //2.7
+#define RMotor_offset 18
 #define LMotor_offset 20
 
 #include <Servo.h> 
@@ -40,21 +40,20 @@ float f_angle=0;
 
 // Control Loop
 float kp = 30; //30;
-float ki = 0.08 ;//0.09;
+float ki = 0.01 ;//0.09;
 float kd = 700 ;//700;
+#define ERR_SUM_MAX 200
 float errSum, dErr, error, lastErr;
 double pid_input, pid_output;
 
-double pid_setpoint=0.0;
-//double pid_setpoint=2.0;
-
+double pid_setpoint=-7.5;
 
 // Loop Frequencies Control
 unsigned long start_time;
 unsigned long prev_control_ms = 0;
-unsigned long control_period_ms = 12; //200 Hz
+unsigned long control_period_ms = 5; //200 Hz
 unsigned long prev_sensor_ms = 0;
-unsigned long sensor_period_ms = 12; //200 Hz
+unsigned long sensor_period_ms = 5; //200 Hz
 unsigned long prev_remote_ms = 0;
 unsigned long remote_period_ms = 200; //5 Hz
 
@@ -76,30 +75,31 @@ void setup() {
   myservo.attach(SERVO);
   
   //Loop Frequencies
-  prev_control_ms = millis();
+  prev_control_ms = millis()+7;
   prev_sensor_ms  = millis();
-  prev_remote_ms  = millis();
+  prev_remote_ms  = millis()+1;
 }
 
 void loop()
-{
-  unsigned long current_millis = millis();
-  unsigned long new_millis = current_millis;
-  
+{  
   //Read & Filter sensor data
   if((millis() - prev_sensor_ms) >= sensor_period_ms) {
+    start_time = millis();
     sensor_loop();
+    prev_sensor_ms = start_time;
   }
 
   //Control
   if((millis() - prev_control_ms) >= control_period_ms) {
+    start_time = millis();
+    
     if(abs(f_angle)<=45){
-//      sensor_loop();
       control_loop();
     } else{
       analogWrite(ENA, 0);
       analogWrite(ENB, 0);
     }
+    prev_control_ms = start_time;
   }
   
   //Read Joystick command/serial command
@@ -110,15 +110,18 @@ void loop()
 }
 
 void control_loop(){
-  start_time = millis();
+  #ifdef DEBUG_MODE
+  unsigned long start_us = micros();
+  #endif
+  
   float dt = (start_time-prev_control_ms);
   //PID
   pid_input = f_angle;
   error = pid_input-pid_setpoint;
   errSum += error * dt;
   //limit errSum:
-  errSum = min(30,errSum);
-  errSum = max(-30,errSum);
+  errSum = min(ERR_SUM_MAX,errSum);
+  errSum = max(-ERR_SUM_MAX,errSum);
   
   dErr = (error - lastErr) / dt;
   pid_output = kp * error + ki * errSum + kd * dErr;
@@ -130,41 +133,51 @@ void control_loop(){
   send_motor_cmd(motor_cmd,motor_cmd);
   
   #ifdef DEBUG_MODE
-  Serial.print("  motor_cmd="); Serial.print(motor_cmd);
-  Serial.print("  dt_control="); Serial.print(dt);
-  Serial.print("  kp=");Serial.print(kp);
-  Serial.print("  kd=");Serial.print(kd);
-  Serial.print("  ki=");Serial.print(ki);
-  Serial.print("  errSum=");Serial.print(errSum);
-  Serial.println();
+//  Serial.print("  motor_cmd="); Serial.print(motor_cmd);
+//  Serial.print("  dt_control="); Serial.print(dt);
+//  Serial.print("  kp=");Serial.print(kp);
+//  Serial.print("  kd=");Serial.print(kd);
+//  Serial.print("  ki=");Serial.print(ki);
+//  Serial.print("  errSum=");Serial.print(errSum);
+
+//  int d = micros()-start_us;
+  int d =start_time - prev_control_ms;
+  Serial.print(" dtc=");Serial.print(d);
   #endif
-  prev_control_ms = start_time;
 }
 
 void sensor_loop(){
-  start_time = millis();
-
-  //Read Accel & Gyro
+  #ifdef DEBUG_MODE
+  unsigned long start_us = micros();
+  #endif
+  
+  //Read Accel & Gyro (1.9ms)
   int16_t ax, ay, az, gx, gy, gz;
-  accelgyro.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
+  accelgyro.getMotion6(&ax, &ay, &az, &gx, &gy, &gz); //this one take 1.
+
+  //convert to theta & theta_dot (0.2ms)
   float r_angle = (atan2(ay, az) * 180 / pi + Angle_offset);
   float omega =  Gyr_Gain * (gx +  Gry_offset);
   
-  //Filter
+  //1st order LPF (0.2ms)
   float dt = (start_time-prev_sensor_ms)/1000.0;//sec
   float K = 0.8;
   float A = K / (K + dt);
   f_angle = A * (f_angle + omega * dt) + (1 - A) * r_angle;
 
+
   #ifdef DEBUG_MODE
-  Serial.print("  omega="); Serial.print(omega);
-  Serial.print("  r_angle="); Serial.print(r_angle);
-  Serial.print("  f_angle=");Serial.print(f_angle);
-  Serial.print("  dt_sensor="); Serial.print(start_time-prev_sensor_ms);
-//  Serial.println();
-  #endif
-  prev_sensor_ms = start_time;    
-  
+//  Serial.print("  omega="); Serial.print(omega);
+//  Serial.print("  r_angle="); Serial.print(r_angle);
+//  Serial.print("  f_angle=");Serial.print(f_angle);
+//  Serial.print("  dt_sensor_loop="); Serial.print(start_time-prev_sensor_ms);
+//  Serial.print("  dts="); Serial.print(micros()-start_us);
+
+//  int d = micros()-start_us;
+  int d =start_time - prev_sensor_ms;
+  Serial.print("  dts="); Serial.print(d);
+  Serial.println();
+  #endif  
 }
 
 //--------------------------------
@@ -222,6 +235,6 @@ void read_servo_config(){
 }
 
 void remote_loop(){
-//  read_pid_config();
+//  read_pid_config(); 
   read_servo_config();
 }
